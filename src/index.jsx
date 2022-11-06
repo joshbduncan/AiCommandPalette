8248048e1b158490436a784849a001e913958cc9
Josh Duncan <44387852+joshbduncan@users.noreply.github.com>
2022-11-06 16:31:55 -0500

    v0.4.3
    - versionCheck function comparisons were incorrect

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,7 @@ See the LICENSE file for details.
   // SCRIPT INFORMATION
 
   var _title = "Ai Command Palette";
-  var _version = "0.4.2";
+  var _version = "0.4.3";
   var _copyright = "Copyright 2022 Josh Duncan";
   var _website = "joshbduncan.com";
   var _github = "https://github.com/joshbduncan";
